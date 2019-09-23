63c472894849366d18d51a041b08a0707a65a271
kaps355 <kapspatel355@gmail.com>
2019-09-23 19:04:12 +0530

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,41 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+    p @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to @product
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to @product
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to @product
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
